2a2199770b55ca068689796ee5338ef4b497f60b
yeledomercy <yeledomercy@gmail.com>
2020-07-30 15:36:09 -0700

    firstpush

---
+++ Faces/Assets/HandCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider) {
+      Debug.Log("Hand collision name: " + collider.gameObject.name);
+
+      if (collider.gameObject.name == "Right Hand Sphere")
+        OVRInput.SetControllerVibration(1, 0.25f, OVRInput.Controller.RTouch);
+      if (collider.gameObject.name == "Left Hand Sphere")
+        OVRInput.SetControllerVibration(1, 0.25f, OVRInput.Controller.LTouch);
+    }
+
+    private void OnTriggerStay(Collider collider) {
+      if (collider.gameObject.name == "Right Hand Sphere")
+        OVRInput.SetControllerVibration(1, 0.25f, OVRInput.Controller.RTouch);
+      if (collider.gameObject.name == "Left Hand Sphere")
+        OVRInput.SetControllerVibration(1, 0.25f, OVRInput.Controller.LTouch);
+    }
+
+    private void OnTriggerExit(Collider collider) {
+      Debug.Log("Hand exit name: " + collider.gameObject.name);
+
+      if (collider.gameObject.name == "Right Hand Sphere")
+        OVRInput.SetControllerVibration(0, 0f, OVRInput.Controller.RTouch);
+      if (collider.gameObject.name == "Left Hand Sphere")
+        OVRInput.SetControllerVibration(0, 0f, OVRInput.Controller.LTouch);
+    }
+}
---
+++ Faces/Assets/SetHead.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetHead : MonoBehaviour
+{
+    public static SetHead instance;
+
+    private void OnEnable()
+    {
+       ChangeHead.headStateChange += OnHeadStateChange;      
+    }
+
+    private void OnDisable()
+    {
+        ChangeHead.headStateChange -= OnHeadStateChange;         
+    }
+
+    private void OnHeadStateChange(string state)
+    {
+        if (gameObject.name == state)
+        {
+            gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
